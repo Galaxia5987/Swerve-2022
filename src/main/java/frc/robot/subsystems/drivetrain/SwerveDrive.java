16e5e3fa2fb20d8fd56f4f2381d702514de8f62e
Barel Shlidor <Barel@shlidor.org>
2021-12-02 11:59:12 +0200

    Create SwerveModules in SwerveDrive subsystem

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
@@ -1,6 +1,17 @@
 package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class SwerveDrive extends SubsystemBase {
+    private final SwerveModule[] modules = new SwerveModule[4];
+    private final boolean fieldOriented;
+
+    public SwerveDrive(boolean fieldOriented) {
+        this.fieldOriented = fieldOriented;
+        modules[0] = new SwerveModule(Constants.SwerveModule.frConfig);
+        modules[1] = new SwerveModule(Constants.SwerveModule.flConfig)  ;
+        modules[2] = new SwerveModule(Constants.SwerveModule.rrConfig);
+        modules[3] = new SwerveModule(Constants.SwerveModule.rlConfig);
+    }
 }
