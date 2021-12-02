8ccf0e52d2445ba670a60c8eab53da5c201f50da
Barel Shlidor <Barel@shlidor.org>
2021-12-02 13:12:54 +0200

    Create getInstance and getModule methods in SwerveDrive

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
@@ -4,14 +4,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class SwerveDrive extends SubsystemBase {
+    private static final SwerveDrive INSTANCE = new SwerveDrive(true);
     private final SwerveModule[] modules = new SwerveModule[4];
     private final boolean fieldOriented;
 
-    public SwerveDrive(boolean fieldOriented) {
+    private SwerveDrive(boolean fieldOriented) {
         this.fieldOriented = fieldOriented;
         modules[0] = new SwerveModule(Constants.SwerveModule.frConfig);
-        modules[1] = new SwerveModule(Constants.SwerveModule.flConfig)  ;
+        modules[1] = new SwerveModule(Constants.SwerveModule.flConfig);
         modules[2] = new SwerveModule(Constants.SwerveModule.rrConfig);
         modules[3] = new SwerveModule(Constants.SwerveModule.rlConfig);
+    }
+
+    /**
+     * @return the swerve.
+     */
+    public static SwerveDrive getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * @param index the index of the module.
+     * @return the corresponding module.
+     */
+    public SwerveModule getModule(int index) {
+        return modules[index];
     }
 }
