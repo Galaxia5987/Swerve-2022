befaa7267187ad8300cd0c615371daf02e80c0ed
Barel Shlidor <Barel@shlidor.org>
2021-12-02 11:24:40 +0200

    Create SwerveDrive and SwerveModule classes in Constants in Ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 
 public final class Constants {
+    public static final class SwerveDrive {
+    }
 
-
+    public static final class SwerveModule {
+    }
 }
--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -5,6 +5,10 @@ public final class Ports {
 
     }
 
+    public static final class SwerveModule {
+
+    }
+
     public static class Controls {
         public static final int XBOX = 0;
     }
