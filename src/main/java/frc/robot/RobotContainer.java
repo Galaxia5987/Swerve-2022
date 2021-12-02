fa58c7fe1b0aae48fa268d968d70ce9d2cde6513
Barel Shlidor <Barel@shlidor.org>
2021-12-02 13:13:35 +0200

    Update absolute/relative mode inRobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,38 +3,63 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+    // The robot's subsystems and commands are defined here...
 
-  private final XboxController xbox = new XboxController(Ports.Controls.XBOX);
-  private final JoystickButton a = new JoystickButton(xbox, XboxController.Button.kA.value);
-
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Configure the button bindings and default commands
-    configureDefaultCommands();
-    configureButtonBindings();
-  }
-
-  private void configureButtonBindings() {
-
-  }
-
-  private void configureDefaultCommands() {
-  }
+    private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
+    private final XboxController xbox = new XboxController(Ports.Controls.XBOX);
+    private final JoystickButton a = new JoystickButton(xbox, XboxController.Button.kA.value);
+    private boolean hasSwerveEncoderReset = false;
 
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
+    /**
+     * The container for the robot.  Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the button bindings and default commands
+        configureDefaultCommands();
+        configureButtonBindings();
+    }
 
-    // An ExampleCommand will run in autonomous
-    return null;
-  }
+    private void configureButtonBindings() {
+
+    }
+
+    private void configureDefaultCommands() {
+    }
+
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+
+        // An ExampleCommand will run in autonomous
+        return null;
+    }
+
+    public void teleopInit() {
+        for (int i = 0; i < 4; i++) {
+            swerveDrive.getModule(i).setEncoderRelative(!hasSwerveEncoderReset);
+        }
+        hasSwerveEncoderReset = true;
+
+    }
+
+    public void autoInit() {
+        for (int i = 0; i < 4; i++) {
+            swerveDrive.getModule(i).setEncoderRelative(!hasSwerveEncoderReset);
+        }
+        hasSwerveEncoderReset = true;
+    }
+
+    public void disableInit() {
+        for (int i = 0; i < 4; i++) {
+            swerveDrive.getModule(i).setEncoderAbsolute();
+        }
+    }
 }
