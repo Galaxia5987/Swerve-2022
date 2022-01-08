2d4066707327e53d9907f8003e44367811e22599
Barel Shlidor <Barel@shlidor.org>
2022-01-08 12:05:55 +0200

    Rename targetSpeed to targetVelocity and add Documentation to DriveForward

--- src/main/java/frc/robot/subsystems/drivetrain/commands/tuning/DriveForward.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/tuning/DriveForward.java
@@ -7,10 +7,14 @@ import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.valuetuner.WebConstant;
 import webapp.FireLog;
 
+/**
+ * The command is responsible for tuning the velocities of the drive motor.
+ * The command rotates the velocities the motor at a specified rate and log it into Fielog.
+ */
 public class DriveForward extends CommandBase {
 
     private final SwerveDrive swerveDrive;
-    private final WebConstant target = new WebConstant("targetSpeed", 0);
+    private final WebConstant target = new WebConstant("targetVelocity", 0);
 
     public DriveForward(SwerveDrive swerveDrive) {
         this.swerveDrive = swerveDrive;
