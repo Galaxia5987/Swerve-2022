f2dd937c34ef28b59b2daf8daa90fbfcfcb4ee8e
Barel Shlidor <Barel@shlidor.org>
2022-01-22 19:45:49 +0200

    Change the purpose of assisedDrive and rename it to PickupCargo

---
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/PickupCargo.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+
+import java.util.function.DoubleSupplier;
+
+
+public class PickupCargo extends CommandBase {
+    private final SwerveDrive swerveDrive;
+    private final DoubleSupplier speed;
+    private final DoubleSupplier cargoYaw;
+
+    /**
+     * Pickup a cargo.
+     *
+     * @param swerveDrive the swerveDrive.
+     * @param speed the speed we should come to the cargo. [m/s]
+     * @param cargoYaw the yaw of the cargo relative to the robot. [degrees]
+     */
+    public PickupCargo(SwerveDrive swerveDrive, DoubleSupplier speed, DoubleSupplier cargoYaw) {
+        this.swerveDrive = swerveDrive;
+        this.speed = speed;
+        this.cargoYaw = cargoYaw;
+        addRequirements(swerveDrive);
+    }
+
+    @Override
+    public void execute() {
+        double currentSpeed = speed.getAsDouble();
+        Rotation2d currentCargoYaw = Rotation2d.fromDegrees(cargoYaw.getAsDouble());
+        double forward = currentSpeed * currentCargoYaw.getCos();
+        double strafe = currentSpeed * currentCargoYaw.getSin();
+        swerveDrive.holonomicDriveKeepSetpoint(forward, strafe, Robot.getAngle()
+                .plus(Rotation2d.fromDegrees(cargoYaw.getAsDouble())));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerveDrive.terminate();
+    }
+}
